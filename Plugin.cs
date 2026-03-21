093cb7c01a760e632669e4b28048d1636caa86c8
TheLazyCowboy1 <Nathan@331oak.com>
2026-03-20 22:49:45 -0400

    PREVIOUS ATTEMPT ACTUALLY WORKED WHAT????

--- Plugin.cs
+++ Plugin.cs
@@ -211,7 +211,7 @@ public partial class Plugin : BaseUnityPlugin
                 {
                     screenLevelTex?.Release();
                     screenLevelTex = MakeRenderTex(src.width, src.height);
-                    Shader.SetGlobalTexture("_TheLazyCowboy1_ScreenLevelTex", screenLevelTex); //don't forget the _ lol
+                    Shader.SetGlobalTexture("_TheLazyCowboy1_ScreenLevelTex", screenLevelTex);
                     PublicLogger.LogDebug("Created TheLazyCowboy1_ScreenLevelTex");
                 }
 

b45952f218b3067310ca7e6e5fdb25381e11b80f
TheLazyCowboy1 <Nathan@331oak.com>
2026-03-20 22:39:55 -0400

    first lazy attempt at true parallax

--- Plugin.cs
+++ Plugin.cs
@@ -211,7 +211,7 @@ public partial class Plugin : BaseUnityPlugin
                 {
                     screenLevelTex?.Release();
                     screenLevelTex = MakeRenderTex(src.width, src.height);
-                    Shader.SetGlobalTexture("_TheLazyCowboy1_ScreenLevelTex", screenLevelTex);
+                    Shader.SetGlobalTexture("_TheLazyCowboy1_ScreenLevelTex", screenLevelTex); //don't forget the _ lol
                     PublicLogger.LogDebug("Created TheLazyCowboy1_ScreenLevelTex");
                 }
 

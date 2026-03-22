9f83652311f0642551ccd7ca24b62068f5cb4456
TheLazyCowboy1 <Nathan@331oak.com>
2026-03-22 19:58:15 -0400

    back to GrabPass method

--- Plugin.cs
+++ Plugin.cs
@@ -174,7 +174,7 @@ public partial class Plugin : BaseUnityPlugin
                 ScreenTexMaterial = new(ScreenTexShader);
                 ScreenTexMaterial.EnableKeyword("THELAZYCOWBOY1_TERRAIN"); //read the terrain too
 
-                Futile.instance.camera.gameObject.AddComponent<ParallaxEffect>();
+                //Futile.instance.camera.gameObject.AddComponent<ParallaxEffect>();
 
             }
             catch (Exception ex) { Logger.LogError(ex); }
@@ -426,9 +426,9 @@ public partial class Plugin : BaseUnityPlugin
 
         //TRUE PARALLAX STUFF
         //add full screen effect to camera
-        //FSprite temp = new FSprite(Futile.whiteElement) { shader = TrueParallaxFShader, width = self.sSize.x, height = self.sSize.y, anchorX = 0, anchorY = 0 };//x = 0.5f*self.sSize.x, y = 0.5f*self.sSize.y },
-        //self.ReturnFContainer("Bloom").AddChild(new BlitScreenTexFNode());
-        //self.ReturnFContainer("Bloom").AddChild(temp);
+        FSprite temp = new(Futile.whiteElement) { shader = TrueParallaxFShader, width = self.sSize.x, height = self.sSize.y, anchorX = 0, anchorY = 0 };//x = 0.5f*self.sSize.x, y = 0.5f*self.sSize.y },
+        self.ReturnFContainer("GrabShaders").AddChild(new BlitScreenTexFNode());
+        self.ReturnFContainer("GrabShaders").AddChild(temp);
 
     }
 
